a7780ebf82e0534d724b485deb6fe6d3aaff01d4
deshanreal <oreokucinglucu99@gmail.com>
2024-12-09 14:32:06 +0700

    Create 1.go

---
+++ 2311102326_Deshan Rafif Alfarisi_Tugas Akhir/1.go
@@ -0,0 +1,24 @@
+package main
+
+import"fmt"
+//type array dengan panjang 2022
+type arrInt[2022]int 
+
+func exist(T set, n2311102326, val int ) bool
+
+
+func inputSet(T *set, n *int )
+
+func findIntersection( T1, T2 n2311102326, m int, T3 *set, h *int)
+
+func printSet(T set, n int )
+
+func main(){
+	var s1, s2, s3 set
+	var n1, n2, n3 int
+	inputSet(&s1,&n1)
+	inputSet(&s2, &n2)
+	findInterception( s1, s2, n1, n2, &s3, &n3)
+	printSet(s3,n3)
+}
+ 
