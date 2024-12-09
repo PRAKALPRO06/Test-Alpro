a5e61252c61ee5b266243832a681fcaf7f090faf
deshanreal <oreokucinglucu99@gmail.com>
2024-12-09 14:32:48 +0700

    Create 5.go

---
+++ 2311102326_Deshan Rafif Alfarisi_Tugas Akhir/5.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const nMax_2311102326 = 1000
+
+//strut partai
+type Partai struct{
+	nama string
+	suara int16
+}
+
+//tipe tabpartai
+type DaftarPartai[]Partai
+
+func main(){
+	//deklarasi var
+	
+}
