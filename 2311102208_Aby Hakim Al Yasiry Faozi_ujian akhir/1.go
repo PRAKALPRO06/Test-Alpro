9a59b81c1ba44cc4c2d7d57bd29a875e2fa5e5a9
AbyHakim05 <2311102208@ittelkom-pwt.ac.id>
2024-12-09 14:31:49 +0700

    Add files via upload

---
+++ 2311102208_Aby Hakim Al Yasiry Faozi_ujian akhir/1.go
@@ -0,0 +1,50 @@
+package main
+import "fmt"
+
+type set2311102208 [2022]int
+
+var true int
+
+// fungsi
+func exist(T set2311102208, n, val int) bool {
+	T = 0
+	val = 1
+	for T > val {
+		n = val
+		return true
+	}
+}
+
+// fungsi untuk menginput
+func inputsize(T set2311102208, n int) {
+	if T > val {
+		return true
+	} else {
+		return false
+	}
+}
+
+//fungsi untuk mencari bilangan
+func findintersction(T1, T2 set2311102208, n, m int, T3 set2311102208, h int) {
+	n, m = val
+	T1, T2 = n, m
+	if n >= T1 {
+		return true
+	}
+}
+
+// fungsi untuk mencetak
+func printset(T int, n int) {
+	for (T = n) {
+		return true
+	}
+}
+
+func main() {
+	var s1, s2, s3 set2311102208
+	var n1, n2, n3 int
+	inputset(&s1, &n1)
+	inputset(&s2, &n2)
+	findintersction(s1, s2, n1, n2, &s3, &n3)
+	print(s3, n3)
+}
---
+++ 2311102208_Aby Hakim Al Yasiry Faozi_ujian akhir/3.go
@@ -0,0 +1,29 @@
+package main
+import "fmt"
+	   "string"
+
+const nProv2311102208 int = 34
+namaProv string
+popProv, tumbuhProv int
+type namaProv = arr[] 
+type popProv = arr[] 
+type tumbuhProv = arr[] 
+
+//input data nama provinsi
+func inputData(prov:namaProv string, pop:popProv int, tumbuh:tumbuhProv int)  {
+	prov = arr[34]
+	provinsi = jateng, jabar, jatim int
+	
+	if (prov >= provinsi) [
+		prov = true
+		return prov
+	] else (prov <= provinsi) {  
+		prov = false
+		return prov
+	}
+}
+
+//menghitung provinsi tercepat
+func ()  {
+	
+}
