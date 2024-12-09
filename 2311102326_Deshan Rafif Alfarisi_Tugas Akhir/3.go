dfa7609d0a1b0865e6bb3f49ac2705c2a347a781
deshanreal <oreokucinglucu99@gmail.com>
2024-12-09 14:32:24 +0700

    Create 3.go

---
+++ 2311102326_Deshan Rafif Alfarisi_Tugas Akhir/3.go
@@ -0,0 +1,17 @@
+package main
+
+import"fmt"
+
+const nProv_2311102326 : int = 34
+type NamaProv = arr[1]string
+type PopProv = arr[1]int
+type TumbuhProv = [1]float64
+
+
+func InputData()
+
+func Provinsi TercepatarrInt()
+
+Func Predisksi()
+
+Func IndeksProvinsi()
